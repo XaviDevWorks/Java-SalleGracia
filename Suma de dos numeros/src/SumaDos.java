83bfdfe56434081ea35c228fd5e22abed1400f23
XavierFarrus <xfarmart@gmail.com>
2024-11-26 09:54:24 +0100

    Add files via upload

---
+++ Suma de dos numeros/src/SumaDos.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumaDos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce el primer número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Introduce el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        int suma = num1 + num2;
+
+        System.out.println("La suma es: " + suma);
+    }
+}
